516bcb6e83def443d0541acbcf424a72e6dd31e2
eborszem <eborszem@purdue.edu>
2023-12-20 13:44:34 -1000

    Testing with guis, now there are multiple buttons

--- Tab.java
+++ Tab.java
@@ -7,29 +7,44 @@ public class Tab implements ActionListener {
 
     int count = 0;
     private JLabel label;
-    private JFrame frame;
-    private JButton button;
+    private JFrame window;
+    //private JButton button;
+    Button[] buttonArr = new Button[5];
 
 
     public Tab() {
         System.out.println("Hello world");
-
-        frame = new JFrame();
-        button = new JButton("click!");
-        button.addActionListener(this);
-        label = new JLabel("num clicks: " + count);
+        window = new JFrame();
+        window.setTitle("Tab to Music");
+        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        window.setSize(800, 500);
+        window.setLocationRelativeTo(null);
 
         JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 100, 300));
-        panel.setLayout(new GridLayout(0, 1));
-        panel.add(button);
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
+        panel.setBackground(Color.GRAY);
+        for (int i = 0; i < 5; i++) {
+            buttonArr[i] = new Button(Integer.toString(i));
+            panel.add(buttonArr[i]);
+            buttonArr[i].addActionListener(this);
+            window.add(panel, BorderLayout.SOUTH);
+        }
+
+        label = new JLabel("num clicks: " + count);
         panel.add(label);
 
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("OUR GUI");
-        frame.pack();
-        frame.setVisible(true);
+        window.setVisible(true);  // seemingly needs to be last
+        
+
+        /*JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 100, 300));
+        panel.setLayout(new GridLayout(5, 7));
+        panel.add(button);*/
+
+
+        //window.add(panel, BorderLayout.CENTER);
+        //window.pack();
+        
     }
 
     public static void main(String[] args) {
@@ -37,7 +52,18 @@ public class Tab implements ActionListener {
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent arg) {
+        if (arg.getSource() == buttonArr[0]) {
+            System.out.println("button 0");
+        } else if (arg.getSource() == buttonArr[1]) {
+            System.out.println("button 1");
+        } else if (arg.getSource() == buttonArr[2]) {
+            System.out.println("button 2");
+        } else if (arg.getSource() == buttonArr[3]) {
+            System.out.println("button 3");
+        } else if (arg.getSource() == buttonArr[4]) {
+            System.out.println("button 4");
+        }
         count++;
         label.setText("Num clicks: " + count);
         //throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
